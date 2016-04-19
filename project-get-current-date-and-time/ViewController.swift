228e8e534ddf3e64e51b4dd030a97fe1acdffa36
reyes <machikoreo@gmail.com>
2016-04-19 17:30:34 +0800

    Initial Commit

---
+++ project-get-current-date-and-time/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  project-get-current-date-and-time
+//
+//  Created by cheng on 2016/4/19.
+//  Copyright © 2016年 reyes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ project-get-current-date-and-timeTests/project_get_current_date_and_timeTests.swift
@@ -0,0 +1,36 @@
+//
+//  project_get_current_date_and_timeTests.swift
+//  project-get-current-date-and-timeTests
+//
+//  Created by cheng on 2016/4/19.
+//  Copyright © 2016年 reyes. All rights reserved.
+//
+
+import XCTest
+@testable import project_get_current_date_and_time
+
+class project_get_current_date_and_timeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
